1a0c4521b9d8e2255a1e271b85ab6fa7d04c6e95
jampingstun <jampingstun@gmail.com>
2012-07-18 00:04:23 +0700

    tampilan menu fix

--- view/header.php
+++ view/header.php
@@ -3,8 +3,24 @@
     <head>
         <link href="assets/css/bootstrap.css" rel="stylesheet">
             <style type="text/css">
- 
-    </style>
+                .submenu li:last-child{
+                    padding-bottom: 3px;
+                    margin-top: 10px;
+                }
+                .submenu li:first-child{
+                    margin-bottom: 2px;
+                    padding-top: 3px;
+                }
+                .submenu li:hover{
+                    margin-left: 4px;
+                    
+                }
+                .menuhead{
+                    font-size: 2em;
+                    font-family: arial;
+                }
+
+            </style>
     </head>
     <body >
     
